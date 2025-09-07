27db1dff637e0ccb7c85db8bf6b43716f6f08873
Nick Brown <nick@mesham.com>
2025-09-07 17:01:22 +0100

    Support for optionally printing the cycle number during execution

--- tt_sim/device/clock.py
+++ tt_sim/device/clock.py
@@ -17,10 +17,12 @@ class Clock:
     def __init__(self, clockables):
         self.clock_items = clockables
 
-    def clock_tick(self):
+    def clock_tick(self, cycle, print_cycle=False):
         for item in self.clock_items:
+            if print_cycle:
+                print(f"[{cycle}]", end="")
             item.clock_tick()
 
-    def run(self, num_iterations):
+    def run(self, num_iterations, print_cycle=False):
         for i in range(num_iterations):
-            self.clock_tick()
+            self.clock_tick(i, print_cycle)
