716acae4a18b7df9480be0579dd71ab5027ad187
Nick Brown <nick@mesham.com>
2025-09-08 16:14:57 +0100

    Tensix general purpose register support

--- tt_sim/pe/tensix/tensix.py
+++ tt_sim/pe/tensix/tensix.py
@@ -3,6 +3,34 @@ from tt_sim.pe.pe import ProcessingElement
 from tt_sim.util.conversion import conv_to_bytes
 
 
+class TensixGPR(MemMapable):
+    def __init__(self, tensix_cp):
+        self.tensix_cp = tensix_cp
+
+    def read(self, addr, size):
+        return conv_to_bytes(0)
+        # raise NotImplementedError(
+        #        (
+        #        f"Reading from address {hex(addr)} not yet supported by tensix "
+        #        f"co-processor backend configuration"
+        #
+        #        )
+        #    )
+
+    def write(self, addr, value, size=None):
+        return
+        # raise NotImplementedError(
+        #        (
+        #        f"Writing to address {hex(addr)} not yet supported by tensix "
+        #        f"co-processor backend configuration"
+        #
+        #        )
+        #    )
+
+    def getSize(self):
+        return 0xFFF
+
+
 class TensixBackendConfiguration(MemMapable):
     def __init__(self, tensix_cp):
         self.tensix_cp = tensix_cp
