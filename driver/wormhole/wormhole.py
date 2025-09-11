a316d836b78891040d8f055015838b2f46f9e544
Nick Brown <nick@mesham.com>
2025-09-11 23:59:06 +0100

    Wormhole driver uses tt_metal layer now, full ex one working

--- driver/wormhole/wormhole.py
+++ driver/wormhole/wormhole.py
@@ -4,6 +4,7 @@ from tt_sim.device.tt_device import Wormhole
 from tt_sim.pe.rv.babyriscv import BabyRISCVCoreType
 from tt_sim.util.conversion import (
     conv_to_bytes,
+    conv_to_int32,
 )
 
 wormhole = Wormhole()
@@ -29,3 +30,41 @@ wormhole.deassert_soft_reset((18, 18), BabyRISCVCoreType.BRISC)
 
 wormhole.reset()
 wormhole.run(3100)
+
+tt_metal.load_kernel("one/parameters.json")
+
+kernel_binaries = tt_metal.get_transfer_kernel_binary_details()
+for kernel_bin in kernel_binaries:
+    wormhole.write((18, 18), kernel_bin[0], kernel_bin[2], kernel_bin[1])
+
+launch_mailbox = tt_metal.generate_transfer_mailbox_details("launch")
+for mailbox_data in launch_mailbox:
+    wormhole.write(
+        (18, 18), mailbox_data[0], conv_to_bytes(mailbox_data[2], mailbox_data[1])
+    )
+
+go_message = tt_metal.generate_transfer_mailbox_details("go_message")
+for go_message_component in go_message:
+    wormhole.write(
+        (18, 18),
+        go_message_component[0],
+        conv_to_bytes(go_message_component[2], go_message_component[1]),
+    )
+
+rt_args = tt_metal.generate_transfer_runtime_arguments_details()
+for rt_arg in rt_args:
+    wormhole.write((18, 18), rt_arg[0], conv_to_bytes(rt_arg[2], rt_arg[1]))
+
+## Write input data to DDR memory
+list1 = list(range(100))
+list2 = [100 - i for i in range(100)]
+wormhole.write((16, 16), 0x20, conv_to_bytes(list1))
+wormhole.write((16, 16), 0x1C0, conv_to_bytes(list2))
+
+## Run 3000 cycles to process the kernel
+wormhole.run(3000)
+
+## Check results in DDR memory are correct
+for i in range(100):
+    val = conv_to_int32(wormhole.read((16, 16), 0x360 + (i * 4), 4))
+    assert val == list1[i] + list2[i]
