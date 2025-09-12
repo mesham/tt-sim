18900d7de5494634ea1e544d4382708d67b30992
Nick Brown <nick@mesham.com>
2025-09-12 19:29:50 +0100

    Tensix mover

--- tt_sim/pe/tensix/tensix.py
+++ tt_sim/pe/tensix/tensix.py
@@ -1,6 +1,21 @@
+from abc import ABC
+from enum import IntEnum
+
 from tt_sim.memory.mem_mapable import MemMapable
+from tt_sim.memory.memory import VisibleMemory
 from tt_sim.pe.pe import ProcessingElement
 from tt_sim.util.conversion import conv_to_bytes, conv_to_uint32
+
+
+class XMOV_DIRECTION(IntEnum):
+    XMOV_L0_TO_L1 = 0
+    XMOV_L1_TO_L0 = 1
+    XMOV_L0_TO_L0 = 2
+    XMOV_L1_TO_L1 = 3
+
+
+TENSIX_CFG_BASE = 0xFFEF0000
+MEM_NCRISC_IRAM_BASE = 0xFFC00000
 
 
 class TensixGPR(MemMapable):
@@ -77,13 +92,25 @@ class TensixBackendConfiguration(MemMapable):
 
 
 class TensixCoProcessor(ProcessingElement, MemMapable):
+    def __init__(self):
+        mover = Mover(self)
+        self.backend = TensixBackend(mover)
+        self.addressable_memory = None
+
+    def setAddressableMemory(self, addressable_memory):
+        if len(addressable_memory) == 1:
+            self.addressable_memory = addressable_memory[0]
+        else:
+            self.addressable_memory = VisibleMemory.merge(*addressable_memory)
+
+    def getBackend(self):
+        return self.backend
+
     def read(self, addr, size):
         return conv_to_bytes(0)
-        # raise NotImplementedError()
 
     def write(self, addr, value, size=None):
         return
-        # raise NotImplementedError()
 
     def getSize(self):
         return 0x2FFFF
@@ -102,3 +129,60 @@ class TensixCoProcessor(ProcessingElement, MemMapable):
 
     def reset(self):
         pass
+
+
+class TensixBackend:
+    def __init__(self, mover):
+        self.mover = mover
+
+    def getMover(self):
+        return self.mover
+
+    def issue_instruction(self, instruction):
+        pass
+
+
+class TensixBackendUnit(ABC):
+    def __init__(self, tensix_coprocessor):
+        self.tensix_coprocessor = tensix_coprocessor
+
+
+class Mover(TensixBackendUnit):
+    def __init__(self, tensix_coprocessor):
+        super().__init__(tensix_coprocessor)
+
+    def move(self, dst, src, count, mode):
+        """
+        This is based on the functional model description at
+        https://github.com/tenstorrent/tt-isa-documentation/blob/main/WormholeB0/TensixTile/Mover.md
+        """
+        assert self.tensix_coprocessor.addressable_memory is not None
+        if mode == XMOV_DIRECTION.XMOV_L1_TO_L1 or mode == XMOV_DIRECTION.XMOV_L0_TO_L1:
+            # In the "_TO_L1" modes, dst must be an address in L1.
+            assert dst < 1024 * 1464
+        else:
+            if dst <= 0xFFFF:
+                dst += TENSIX_CFG_BASE
+            elif 0x40000 <= dst and dst <= 0x4FFFF:
+                dst = (dst - 0x40000) + MEM_NCRISC_IRAM_BASE
+            else:
+                dst = None  # Operation still happens, but the writes get discarded.
+
+            if (dst & 0xFFFF) + count > 0x10000:
+                raise NotImplementedError(
+                    "Can not access more than one region at a time"
+                )
+
+        # Perform the operation.
+        if mode == XMOV_DIRECTION.XMOV_L1_TO_L1 or mode == XMOV_DIRECTION.XMOV_L1_TO_L0:
+            # In the "L1_TO_" modes, a memcpy is done, and src must be an address in L1.
+            if src >= (1024 * 1464):
+                raise NotImplementedError("")
+            # print(f"Write to {hex(dst)} from {hex(src)} elements {hex(count)}")
+            self.tensix_coprocessor.addressable_memory.write(
+                dst, self.tensix_coprocessor.addressable_memory.read(src, count)
+            )
+        else:
+            # In the "L0_TO_" modes, a memset is done.
+            zero_val = conv_to_bytes(0, count)
+            self.tensix_coprocessor.addressable_memory.write(dst, zero_val)
