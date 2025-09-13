1c9404ccfa7a7b275a88f8b2d423a04eb9415524
Nick Brown <nick@mesham.com>
2025-09-13 10:22:31 +0100

    Reorg of wormhole driver to make easier to run other problems

--- driver/wormhole/wormhole.py
+++ driver/wormhole/wormhole.py
@@ -8,69 +8,90 @@ from tt_sim.util.conversion import (
     conv_to_uint32,
 )
 
-wormhole = Wormhole()
 
-tt_metal = TT_Metal("tt_metal_0.62.2.json")
+def launch_firmware(wormhole, tt_metal):
+    go_signal_start_addr, go_signal_byte_len = tt_metal.get_mailbox_config_details(
+        "go_message", "signal"
+    )
+    run_msg_done = tt_metal.get_constant("RUN_MSG_DONE")
+    run_msg_go = tt_metal.get_constant("RUN_MSG_GO")
+    firmware_package = tt_metal.read_firmware("firmware")
 
-go_signal_start_addr, go_signal_byte_len = tt_metal.get_mailbox_config_details(
-    "go_message", "signal"
-)
-run_msg_done = tt_metal.get_constant("RUN_MSG_DONE")
-run_msg_go = tt_metal.get_constant("RUN_MSG_GO")
-firmware_package = tt_metal.read_firmware("firmware")
+    # TT Metal sets up a mapping table from DRAM bank to tile for each NoC, this is then
+    # looked up when calling get_noc_addr_from_bank_id
+    dram_to_noc_xy_transfers = tt_metal.generate_dram_noc_mapping_transfers()
+    for data_transfer in dram_to_noc_xy_transfers:
+        wormhole.write(
+            (18, 18),
+            data_transfer[0],
+            conv_to_bytes(data_transfer[2], data_transfer[1]),
+            data_transfer[1],
+        )
 
-# TT Metal sets up a mapping table from DRAM bank to tile for each NoC, this is then
-# looked up when calling get_noc_addr_from_bank_id
-dram_to_noc_xy_transfers = tt_metal.generate_dram_noc_mapping_transfers()
-for data_transfer in dram_to_noc_xy_transfers:
+    # Write firmware binaries to correct locations in tensix
+    for firmware in firmware_package:
+        wormhole.write((18, 18), firmware.get_text_addr(), firmware.get_text_bin())
+        if firmware.get_data_addr() is not None and firmware.get_data_bin() is not None:
+            wormhole.write((18, 18), firmware.get_data_addr(), firmware.get_data_bin())
+
+    # Set jal for BRISC to jump to its firmware from 0x0 start point
     wormhole.write(
         (18, 18),
-        data_transfer[0],
-        conv_to_bytes(data_transfer[2], data_transfer[1]),
-        data_transfer[1],
+        tt_metal.get_config_value("l1_memory_map", "MEM_BOOT_CODE_BASE"),
+        conv_to_bytes(0x7800306F),
     )
 
-# Write firmware binaries to correct locations in tensix
-for firmware in firmware_package:
-    wormhole.write((18, 18), firmware.get_text_addr(), firmware.get_text_bin())
-    if firmware.get_data_addr() is not None and firmware.get_data_bin() is not None:
-        wormhole.write((18, 18), firmware.get_data_addr(), firmware.get_data_bin())
+    wormhole.assert_soft_reset()
+    wormhole.deassert_soft_reset((18, 18), BabyRISCVCoreType.BRISC)
 
-# Set jal for BRISC to jump to its firmware from 0x0 start point
-wormhole.write(
-    (18, 18),
-    tt_metal.get_config_value("l1_memory_map", "MEM_BOOT_CODE_BASE"),
-    conv_to_bytes(0x7800306F),
-)
+    wormhole.reset()
 
-wormhole.assert_soft_reset()
-wormhole.deassert_soft_reset((18, 18), BabyRISCVCoreType.BRISC)
+    # Set the go signal and then loop round whilst this is not done, the firmware will
+    # set it to be done when it has finished and is ready for a kernel
+    wormhole.write(
+        (18, 18), go_signal_start_addr, conv_to_bytes(run_msg_go, go_signal_byte_len)
+    )
+    while (
+        conv_to_uint32(
+            wormhole.read((18, 18), go_signal_start_addr, go_signal_byte_len)
+        )
+        != run_msg_done
+    ):
+        wormhole.run(100)
 
-wormhole.reset()
 
-# Set the go signal and then loop round whilst this is not done, the firmware will
-# set it to be done when it has finished and is ready for a kernel
-wormhole.write(
-    (18, 18), go_signal_start_addr, conv_to_bytes(run_msg_go, go_signal_byte_len)
-)
-while (
-    conv_to_uint32(wormhole.read((18, 18), go_signal_start_addr, go_signal_byte_len))
-    != run_msg_done
-):
-    wormhole.run(100)
+def run_kernel(wormhole, tt_metal, parameters):
+    go_signal_start_addr, go_signal_byte_len = tt_metal.get_mailbox_config_details(
+        "go_message", "signal"
+    )
+    run_msg_done = tt_metal.get_constant("RUN_MSG_DONE")
 
-tt_metal.load_kernel("two/parameters.json")
+    tt_metal.load_kernel(parameters)
 
-# Grab the data transfers needed to the device (includes the kernel binaries,
-# mailbox values, go message, runtime arguments, cb configurations) and
-# write them to the memory of the tensix core
-data_transfers = tt_metal.generate_kernel_to_device_data_transfer_details()
-for data_transfer in data_transfers:
-    if not isinstance(data_transfer[2], bytes):
-        d = conv_to_bytes(data_transfer[2], data_transfer[1])
-    else:
-        d = data_transfer[2]
-    wormhole.write((18, 18), data_transfer[0], d, data_transfer[1])
+    # Grab the data transfers needed to the device (includes the kernel binaries,
+    # mailbox values, go message, runtime arguments, cb configurations) and
+    # write them to the memory of the tensix core
+    data_transfers = tt_metal.generate_kernel_to_device_data_transfer_details()
+    for data_transfer in data_transfers:
+        if not isinstance(data_transfer[2], bytes):
+            d = conv_to_bytes(data_transfer[2], data_transfer[1])
+        else:
+            d = data_transfer[2]
+        wormhole.write((18, 18), data_transfer[0], d, data_transfer[1])
+
+    ## Run cycles to process the kernel, continue until MSG_DONE is written
+    while (
+        conv_to_uint32(
+            wormhole.read((18, 18), go_signal_start_addr, go_signal_byte_len)
+        )
+        != run_msg_done
+    ):
+        wormhole.run(100)
+
+
+wormhole = Wormhole()
+tt_metal = TT_Metal("tt_metal_0.62.2.json")
+launch_firmware(wormhole, tt_metal)
 
 ## Write input data to DDR memory
 list1 = list(range(100))
@@ -78,12 +99,7 @@ list2 = [100 - i for i in range(100)]
 wormhole.write((16, 16), 0x20, conv_to_bytes(list1))
 wormhole.write((16, 16), 0x1C0, conv_to_bytes(list2))
 
-## Run cycles to process the kernel, continue until MSG_DONE is written
-while (
-    conv_to_uint32(wormhole.read((18, 18), go_signal_start_addr, go_signal_byte_len))
-    != run_msg_done
-):
-    wormhole.run(100)
+run_kernel(wormhole, tt_metal, "two/parameters.json")
 
 ## Check results in DDR memory are correct
 for i in range(100):
