f5cee58e2c67c0923570545671270184730d9be1
Nick Brown <nick@mesham.com>
2025-09-14 13:18:51 +0100

    SFPU ignores three opcodes issued to it for now

--- tt_sim/pe/tensix/backends/vector.py
+++ tt_sim/pe/tensix/backends/vector.py
@@ -2,8 +2,20 @@ from tt_sim.pe.tensix.backends.backend_base import TensixBackendUnit
 
 
 class VectorUnit(TensixBackendUnit):
-    def __init__(self, backend):
-        super().__init__(backend)
+    OPCODE_TO_HANDLER = {
+        "SFPENCC": "handle_sfpencc",
+        "SFPLOADI": "handle_sfploadi",
+        "SFPCONFIG": "handle_sfpconfig",
+    }
 
-    def clock_tick(self, cycle_num):
+    def __init__(self, backend):
+        super().__init__(backend, VectorUnit.OPCODE_TO_HANDLER, "Vector")
+
+    def handle_sfpencc(self, instruction_info, issue_thread, instr_args):
+        pass
+
+    def handle_sfploadi(self, instruction_info, issue_thread, instr_args):
+        pass
+
+    def handle_sfpconfig(self, instruction_info, issue_thread, instr_args):
         pass
