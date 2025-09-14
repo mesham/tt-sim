2717e5c4beb307f9e24bd27aa78d1f5ccf00c919
Nick Brown <nick@mesham.com>
2025-09-14 13:18:20 +0100

    Moved backend clock_tick method into backendUnit and will then call into appropriate handler method

--- tt_sim/pe/tensix/backends/backend_base.py
+++ tt_sim/pe/tensix/backends/backend_base.py
@@ -4,9 +4,11 @@ from tt_sim.device.clock import Clockable
 
 
 class TensixBackendUnit(Clockable, ABC):
-    def __init__(self, backend):
+    def __init__(self, backend, opcode_to_method_map, unit_name):
         self.backend = backend
         self.instruction_buffer = []
+        self.opcode_to_method_map = opcode_to_method_map
+        self.unit_name = unit_name
 
     def issueInstruction(self, instruction, from_thread):
         self.instruction_buffer.append(
@@ -15,6 +17,26 @@ class TensixBackendUnit(Clockable, ABC):
                 from_thread,
             )
         )
+
+    def clock_tick(self, cycle_num):
+        if len(self.instruction_buffer) > 0:
+            instruction, issue_thread = self.instruction_buffer.pop(0)
+            instruction_info = (
+                self.backend.tensix_instruction_decoder.getInstructionInfo(instruction)
+            )
+            instruction_name = instruction_info["name"]
+            if instruction_name in self.opcode_to_method_map:
+                if "instr_args" in instruction_info:
+                    instr_args = instruction_info["instr_args"]
+                else:
+                    instr_args = None
+                getattr(self, self.opcode_to_method_map[instruction_name])(
+                    instruction_info, issue_thread, instr_args
+                )
+            else:
+                raise NotImplementedError(
+                    f"{self.unit_name} unit can not handle instruction '{instruction_info['name']}'"
+                )
 
     def getThreadConfigValue(self, issue_thread, key):
         return self.backend.getThreadConfigValue(issue_thread, key)
