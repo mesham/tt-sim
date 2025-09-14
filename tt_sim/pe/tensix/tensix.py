e0e479410f95817eecd62511672e12ffc96b6513
Nick Brown <nick@mesham.com>
2025-09-14 11:58:13 +0100

    Configuration constants loaded from YAML file and provided, along with helper methods

--- tt_sim/pe/tensix/tensix.py
+++ tt_sim/pe/tensix/tensix.py
@@ -2,86 +2,41 @@ import importlib.resources as resources
 
 import yaml
 
-from tt_sim.memory.mem_mapable import MemMapable
 from tt_sim.memory.memory import VisibleMemory
 from tt_sim.pe.pe import ProcessingElement
 from tt_sim.pe.tensix.backend import TensixBackend
 from tt_sim.pe.tensix.frontend import TensixFrontend
 from tt_sim.util.bits import extract_bits, get_bits
-from tt_sim.util.conversion import conv_to_bytes, conv_to_uint32
 
 
-class TensixGPR(MemMapable):
-    def __init__(self, tensix_cp):
-        self.tensix_cp = tensix_cp
+class TensixConfigurationConstants:
+    def __init__(self):
+        with (
+            resources.files("tt_sim.pe.tensix")
+            .joinpath("tensix_backend_cfg.yaml")
+            .open("r") as f
+        ):
+            self.config_constants = yaml.safe_load(f)
 
-    def read(self, addr, size):
-        return conv_to_bytes(0)
-        # raise NotImplementedError(
-        #        (
-        #        f"Reading from address {hex(addr)} not yet supported by tensix "
-        #        f"co-processor backend configuration"
-        #
-        #        )
-        #    )
+    def get_addr32(self, value):
+        assert value in self.config_constants
+        return self.config_constants[value]["ADDR32"]
 
-    def write(self, addr, value, size=None):
-        return
-        # raise NotImplementedError(
-        #        (
-        #        f"Writing to address {hex(addr)} not yet supported by tensix "
-        #        f"co-processor backend configuration"
-        #
-        #        )
-        #    )
+    def get_shamt(self, value):
+        assert value in self.config_constants
+        return self.config_constants[value]["SHAMT"]
 
-    def getSize(self):
-        return 0xFFF
+    def get_mask(self, value):
+        assert value in self.config_constants
+        return self.config_constants[value]["MASK"]
 
+    def parse_raw_config_value(self, value, key):
+        mask = self.get_mask(value)
+        shamt = self.get_shamt(value)
+        return self.tensix_be_config_parse_value(value, shamt, mask)
 
-class TensixBackendConfiguration(MemMapable):
-    CFG_STATE_SIZE = 47
-    THD_STATE_SIZE = 57
-
-    def __init__(self, tensix_cp):
-        self.tensix_cp = tensix_cp
-        self.config = [[0] * TensixBackendConfiguration.CFG_STATE_SIZE * 4] * 2
-        self.threadConfig = [[0] * TensixBackendConfiguration.THD_STATE_SIZE] * 3
-
-    def read(self, addr, size):
-        threadConfigStart = TensixBackendConfiguration.CFG_STATE_SIZE * 4 * 2
-        idx = addr / 4
-        if idx < threadConfigStart:
-            each_config_size = TensixBackendConfiguration.CFG_STATE_SIZE * 4
-            second_idx = 1 if idx > TensixBackendConfiguration.CFG_STATE_SIZE * 4 else 0
-            first_idx = int(idx - (each_config_size * second_idx))
-            return conv_to_bytes(self.config[second_idx][first_idx])
-        else:
-            idx = idx - threadConfigStart
-            second_idx = idx / TensixBackendConfiguration.THD_STATE_SIZE
-            return conv_to_bytes(
-                self.threadConfig[second_idx][
-                    idx - ((TensixBackendConfiguration.THD_STATE_SIZE) * second_idx)
-                ]
-            )
-
-    def write(self, addr, value, size=None):
-        threadConfigStart = TensixBackendConfiguration.CFG_STATE_SIZE * 4 * 2
-        idx = addr / 4
-        if idx < threadConfigStart:
-            each_config_size = TensixBackendConfiguration.CFG_STATE_SIZE * 4
-            second_idx = 1 if idx > each_config_size else 0
-            first_idx = int(idx - (each_config_size * second_idx))
-            self.config[second_idx][first_idx] = conv_to_uint32(value)
-        else:
-            idx = idx - threadConfigStart
-            second_idx = int(idx / TensixBackendConfiguration.THD_STATE_SIZE)
-            self.threadConfig[second_idx][
-                idx - ((TensixBackendConfiguration.THD_STATE_SIZE) * second_idx)
-            ] = conv_to_uint32(value)
-
-    def getSize(self):
-        return 0xFFFF
+    def tensix_be_config_parse_value(self, value, shamt, mask):
+        return (value & mask) >> shamt
 
 
 class TensixInstructionDecoder:
@@ -132,7 +87,10 @@ class TensixInstructionDecoder:
 class TensixCoProcessor(ProcessingElement):
     def __init__(self):
         self.tensix_instruction_decoder = TensixInstructionDecoder()
-        self.backend = TensixBackend(self.tensix_instruction_decoder)
+        self.configuration_constants = TensixConfigurationConstants()
+        self.backend = TensixBackend(
+            self.tensix_instruction_decoder, self.configuration_constants
+        )
         self.threads = [
             TensixFrontend(i, self.tensix_instruction_decoder, self.backend)
             for i in range(3)
