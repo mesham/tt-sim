97fed4c64f03ca3c6fd9c37382a4fe14cbefd3b3
Nick Brown <nick@mesham.com>
2025-09-18 09:56:54 +0100

    Support in thcon for improved setdmareg, reg2flop and storereg

--- tt_sim/pe/tensix/backends/thcon.py
+++ tt_sim/pe/tensix/backends/thcon.py
@@ -1,4 +1,6 @@
 from tt_sim.pe.tensix.backends.backend_base import TensixBackendUnit
+from tt_sim.util.bits import get_bits, get_nth_bit
+from tt_sim.util.conversion import conv_to_bytes
 
 
 class ScalarUnit(TensixBackendUnit):
@@ -8,17 +10,126 @@ class ScalarUnit(TensixBackendUnit):
         "STOREREG": "handle_storereg",
     }
 
+    GLOBAL_CFGREG_BASE_ADDR32 = 152
+    THCON_CFGREG_BASE_ADDR32 = 52
+
     def __init__(self, backend, gprs):
         self.gprs = gprs
         super().__init__(backend, ScalarUnit.OPCODE_TO_HANDLER, "Scalar")
 
     def handle_reg2flop(self, instruction_info, issue_thread, instr_args):
-        # TODO
-        pass
+        inputReg = instr_args["RegIndex"]
+        thConCfgIndex = instr_args["FlopIndex"]
+        targetSel = instr_args["TargetSel"]
+        sizeSel = instr_args["SizeSel"]
+        shift8 = instr_args["ByteOffset"]
+        threadSel = instr_args["ContextId_2"]
+
+        stateID = self.backend.getThreadConfigValue(
+            issue_thread, "CFG_STATE_ID_StateID"
+        )
+
+        assert thConCfgIndex < (
+            ScalarUnit.GLOBAL_CFGREG_BASE_ADDR32 - ScalarUnit.THCON_CFGREG_BASE_ADDR32
+        )
+
+        if targetSel == 0x0:
+            # Move from GPRs to THCON configuration
+            if sizeSel == 0:
+                # 128 bit configuration write
+                for i in range(4):
+                    self.backend.getConfigUnit().setConfig(
+                        stateID,
+                        (thConCfgIndex + ScalarUnit.THCON_CFGREG_BASE_ADDR32) + i,
+                        self.gprs.getRegisters(issue_thread)[inputReg + i],
+                    )
+            else:
+                # 32 bit configuration write
+                self.backend.getConfigUnit().setConfig(
+                    stateID,
+                    thConCfgIndex + ScalarUnit.THCON_CFGREG_BASE_ADDR32,
+                    self.gprs.getRegisters(issue_thread)[inputReg],
+                )
+        else:
+            # Move from GPRs to ADCs
+            overrideThread = get_nth_bit(targetSel, 0)
+
+            xyzw = get_bits(thConCfgIndex, 0, 1)
+            cr = get_bits(thConCfgIndex, 2, 2)
+            adcsel = get_bits(thConCfgIndex, 3, 4)
+            channel = get_bits(thConCfgIndex, 5, 5)
+
+            value = self.gprs.getRegisters(issue_thread)[inputReg]
+
+            match sizeSel:
+                case 0:
+                    # 128 bit
+                    value = 0
+                case 1:
+                    # 32 bit
+                    if shift8 != 0:
+                        value = 0
+                case 2:
+                    # 16 bit
+                    if shift8 == 0:
+                        value &= 0xFFFF
+                    elif shift8 == 2:
+                        value >>= 16
+                    else:
+                        value = 0
+                case 3:
+                    # 8 bit
+                    value = (value >> (shift8 * 8)) & 0xFF
+
+            if overrideThread:
+                whichThread = threadSel
+                if whichThread >= 3:
+                    return
+            else:
+                whichThread = issue_thread
+
+            match adcsel:
+                case 0:
+                    adc = self.backend.getADC(whichThread).Unpacker[0]
+                case 1:
+                    adc = self.backend.getADC(whichThread).Unpacker[1]
+                case 2:
+                    adc = self.backend.getADC(whichThread).Packers
+                case _:
+                    return
+
+            tgt_channel = adc.Channel[channel]
+
+            match xyzw:
+                case 0:
+                    if cr:
+                        tgt_channel.X_Cr = value
+                    else:
+                        tgt_channel.X = value
+                case 1:
+                    if cr:
+                        tgt_channel.Y_Cr = value
+                    else:
+                        tgt_channel.Y = value
+                case 2:
+                    if cr:
+                        tgt_channel.Z_Cr = value
+                    else:
+                        tgt_channel.Z = value
+                case 3:
+                    if cr:
+                        tgt_channel.W_Cr = value
+                    else:
+                        tgt_channel.W = value
 
     def handle_storereg(self, instruction_info, issue_thread, instr_args):
-        # TODO
-        pass
+        addrLo = instr_args["RegAddr"]
+        dataReg = instr_args["TdmaDataRegIndex"]
+
+        addr = 0xFFB00000 + (addrLo << 2)
+        self.backend.getAddressableMemory().write(
+            addr, conv_to_bytes(self.gprs.getRegisters(issue_thread)[dataReg])
+        )
 
     def handle_setdmareg(self, instruction_info, issue_thread, instr_args):
         setSignalsMode = instr_args["SetSignalsMode"]
@@ -30,9 +141,14 @@ class ScalarUnit(TensixBackendUnit):
 
             newValue = newValue1 | (newValue2 << 14)
 
-            existing_val = self.gprs.getRegisters(issue_thread)[int(resultHalfReg / 2)]
-            self.gprs.getRegisters(issue_thread)[int(resultHalfReg / 2)] = (
-                existing_val & 0xFF00
-            ) | newValue
+            base_reg = int(resultHalfReg / 2)
+
+            existing_val = self.gprs.getRegisters(issue_thread)[base_reg]
+            if resultHalfReg & 0x1 != 0:
+                nv = (existing_val & 0xFFFF) | (newValue << 16)
+            else:
+                nv = (existing_val & 0xFFFF0000) | newValue
+
+            self.gprs.getRegisters(issue_thread)[base_reg] = nv
         else:
-            pass
+            raise NotImplementedError()
