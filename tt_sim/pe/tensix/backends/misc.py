675448b54531065b6cd418bd5a673d74f4ecbd32
Nick Brown <nick@mesham.com>
2025-09-21 13:12:59 +0100

    INCADCXY, INCADCZW, ADDRCRXY, ADDRCRZW instructions

--- tt_sim/pe/tensix/backends/misc.py
+++ tt_sim/pe/tensix/backends/misc.py
@@ -7,8 +7,12 @@ class MiscellaneousUnit(TensixBackendUnit):
         "SETADCXY": "handle_setadcxy",
         "SETADCZW": "handle_setadczw",
         "SETADCXX": "handle_setadcxx",
+        "INCADCXY": "handle_incadcxy",
+        "INCADCZW": "handle_incadczw",
         "DMANOP": "handle_dmanop",
         "SETADC": "handle_setadc",
+        "ADDRCRXY": "handle_addrcrxy",
+        "ADDRCRZW": "handle_addrcrzw",
     }
 
     def __init__(self, backend):
@@ -31,6 +35,172 @@ class MiscellaneousUnit(TensixBackendUnit):
         # This is a nop (but in documentation says for the scalar unit, but it is
         # directed to the misc unit for some reason)
         pass
+
+    def handle_addrcrzw(self, instruction_info, issue_thread, instr_args):
+        def apply_to(adc_channel, enables, Z0Inc, W0Inc, Z1Inc, W1Inc):
+            if get_nth_bit(enables, 0):
+                adc_channel.Channel[0].Z_Cr = Z0Inc
+                adc_channel.Channel[0].Z = Z0Inc
+
+            if get_nth_bit(enables, 1):
+                adc_channel.Channel[0].W_Cr = W0Inc
+                adc_channel.Channel[0].W = W0Inc
+
+            if get_nth_bit(enables, 2):
+                adc_channel.Channel[1].Z_Cr = Z1Inc
+                adc_channel.Channel[1].Z = Z1Inc
+
+            if get_nth_bit(enables, 3):
+                adc_channel.Channel[1].W_Cr = W1Inc
+                adc_channel.Channel[1].W = W1Inc
+
+        Z0Inc = instr_args["Ch0_X"]
+        W0Inc = instr_args["Ch0_Y"]
+        Z1Inc = instr_args["Ch1_X"]
+        W1Inc = extract_bits(instr_args["Ch1_Y"], 3, 0)
+        enables = instr_args["BitMask"]
+        threadOverride = extract_bits(instr_args["Ch1_Y"], 2, 3)
+
+        whichThread = issue_thread if threadOverride == 0 else threadOverride - 1
+
+        if get_nth_bit(instr_args["CntSetMask"], 0):
+            apply_to(
+                self.backend.getADC(whichThread).Unpacker[0],
+                enables,
+                Z0Inc,
+                W0Inc,
+                Z1Inc,
+                W1Inc,
+            )
+        if get_nth_bit(instr_args["CntSetMask"], 1):
+            apply_to(
+                self.backend.getADC(whichThread).Unpacker[1],
+                enables,
+                Z0Inc,
+                W0Inc,
+                Z1Inc,
+                W1Inc,
+            )
+        if get_nth_bit(instr_args["CntSetMask"], 2):
+            apply_to(
+                self.backend.getADC(whichThread).Packers,
+                enables,
+                Z0Inc,
+                W0Inc,
+                Z1Inc,
+                W1Inc,
+            )
+
+    def handle_addrcrxy(self, instruction_info, issue_thread, instr_args):
+        def apply_to(adc_channel, enables, X0Inc, Y0Inc, X1Inc, Y1Inc):
+            if get_nth_bit(enables, 0):
+                adc_channel.Channel[0].X_Cr = X0Inc
+                adc_channel.Channel[0].X = X0Inc
+
+            if get_nth_bit(enables, 1):
+                adc_channel.Channel[0].Y_Cr = Y0Inc
+                adc_channel.Channel[0].Y = Y0Inc
+
+            if get_nth_bit(enables, 2):
+                adc_channel.Channel[1].X_Cr = X1Inc
+                adc_channel.Channel[1].X = X1Inc
+
+            if get_nth_bit(enables, 3):
+                adc_channel.Channel[1].Y_Cr = Y1Inc
+                adc_channel.Channel[1].Y = Y1Inc
+
+        X0Inc = instr_args["Ch0_X"]
+        Y0Inc = instr_args["Ch0_Y"]
+        X1Inc = instr_args["Ch1_X"]
+        Y1Inc = extract_bits(instr_args["Ch1_Y"], 3, 0)
+        enables = instr_args["BitMask"]
+        threadOverride = extract_bits(instr_args["Ch1_Y"], 2, 3)
+
+        whichThread = issue_thread if threadOverride == 0 else threadOverride - 1
+
+        if get_nth_bit(instr_args["CntSetMask"], 0):
+            apply_to(
+                self.backend.getADC(whichThread).Unpacker[0],
+                enables,
+                X0Inc,
+                Y0Inc,
+                X1Inc,
+                Y1Inc,
+            )
+        if get_nth_bit(instr_args["CntSetMask"], 1):
+            apply_to(
+                self.backend.getADC(whichThread).Unpacker[1],
+                enables,
+                X0Inc,
+                Y0Inc,
+                X1Inc,
+                Y1Inc,
+            )
+        if get_nth_bit(instr_args["CntSetMask"], 2):
+            apply_to(
+                self.backend.getADC(whichThread).Packers,
+                enables,
+                X0Inc,
+                Y0Inc,
+                X1Inc,
+                Y1Inc,
+            )
+
+    def handle_incadczw(self, instruction_info, issue_thread, instr_args):
+        def apply_to(adc_channel, Z0Inc, W0Inc, Z1Inc, W1Inc):
+            adc_channel.Channel[0].Z += Z0Inc
+            adc_channel.Channel[0].W += W0Inc
+            adc_channel.Channel[1].Z += Z1Inc
+            adc_channel.Channel[1].W += W1Inc
+
+        Z0Inc = instr_args["Ch0_X"]
+        W0Inc = instr_args["Ch0_Y"]
+        Z1Inc = instr_args["Ch1_X"]
+        W1Inc = extract_bits(instr_args["Ch1_Y"], 3, 0)
+
+        threadOverride = extract_bits(instr_args["Ch1_Y"], 2, 3)
+        whichThread = issue_thread if threadOverride == 0 else threadOverride - 1
+
+        if get_nth_bit(instr_args["CntSetMask"], 0):
+            apply_to(
+                self.backend.getADC(whichThread).Unpacker[0], Z0Inc, W0Inc, Z1Inc, W1Inc
+            )
+        if get_nth_bit(instr_args["CntSetMask"], 1):
+            apply_to(
+                self.backend.getADC(whichThread).Unpacker[1], Z0Inc, W0Inc, Z1Inc, W1Inc
+            )
+        if get_nth_bit(instr_args["CntSetMask"], 2):
+            apply_to(
+                self.backend.getADC(whichThread).Packers, Z0Inc, W0Inc, Z1Inc, W1Inc
+            )
+
+    def handle_incadcxy(self, instruction_info, issue_thread, instr_args):
+        def apply_to(adc_channel, X0Inc, Y0Inc, X1Inc, Y1Inc):
+            adc_channel.Channel[0].X += X0Inc
+            adc_channel.Channel[0].Y += Y0Inc
+            adc_channel.Channel[1].X += X1Inc
+            adc_channel.Channel[1].Y += Y1Inc
+
+        X0Inc = instr_args["Ch0_X"]
+        Y0Inc = instr_args["Ch0_Y"]
+        X1Inc = instr_args["Ch1_X"]
+        Y1Inc = extract_bits(instr_args["Ch1_Y"], 3, 0)
+
+        threadOverride = extract_bits(instr_args["Ch1_Y"], 2, 3)
+        whichThread = issue_thread if threadOverride == 0 else threadOverride - 1
+
+        if get_nth_bit(instr_args["CntSetMask"], 0):
+            apply_to(
+                self.backend.getADC(whichThread).Unpacker[0], X0Inc, Y0Inc, X1Inc, Y1Inc
+            )
+        if get_nth_bit(instr_args["CntSetMask"], 1):
+            apply_to(
+                self.backend.getADC(whichThread).Unpacker[1], X0Inc, Y0Inc, X1Inc, Y1Inc
+            )
+        if get_nth_bit(instr_args["CntSetMask"], 2):
+            apply_to(
+                self.backend.getADC(whichThread).Packers, X0Inc, Y0Inc, X1Inc, Y1Inc
+            )
 
     def handle_setadcxx(self, instruction_info, issue_thread, instr_args):
         def apply_to(adc_channel, X0Val, X1Val):
