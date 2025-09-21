3007b4d006cfcfcf3faae382fc48b5e9cec23ad8
Nick Brown <nick@mesham.com>
2025-09-21 14:48:31 +0100

    Message on completion of ex four

--- driver/wormhole/four/four.py
+++ driver/wormhole/four/four.py
@@ -21,3 +21,5 @@ run_kernel(wormhole, tt_metal, "four/parameters.json")
 for i in range(256):
     val = conv_to_int32(wormhole.read((16, 16), 0x220 + (i * 4), 4))
     assert val == list1[i] + list2[i]
+
+print("Example four completed successfully")
