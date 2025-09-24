a4de3a928288803a8d557437d75c601ac52f2636
Nick Brown <nick@mesham.com>
2025-09-24 16:43:49 +0100

    FP version of fifth example using FP for SFPU operations

---
+++ driver/wormhole/five-fp/five-fp.py
@@ -0,0 +1,25 @@
+from driver.wormhole.tt_metal import TT_Metal
+from driver.wormhole.wormhole_driver import launch_firmware, run_kernel
+from tt_sim.device.tt_device import Wormhole
+from tt_sim.util.conversion import (
+    conv_to_bytes,
+    conv_to_float,
+)
+
+wormhole = Wormhole()
+tt_metal = TT_Metal("tt_metal_0.62.2.json")
+launch_firmware(wormhole, tt_metal)
+
+list1 = [float(i) for i in range(256)]
+list2 = [float(255 - i) for i in range(256)]
+wormhole.write((16, 16), 0x20, conv_to_bytes(list1, 4))
+wormhole.write((16, 16), 0x420, conv_to_bytes(list2, 4))
+
+run_kernel(wormhole, tt_metal, "five-fp/parameters.json")
+
+## Check results in DDR memory are correct
+for i in range(256):
+    val = conv_to_float(wormhole.read((16, 16), 0x820 + (i * 4), 4))
+    assert val == list1[i] + list2[i]
+
+print("Example five completed successfully")
