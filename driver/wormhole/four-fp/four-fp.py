7ad5b77e94361bb898996147515a4ff23e735456
Nick Brown <nick@mesham.com>
2025-09-26 13:30:30 +0100

    Examples have diagnostics included to show how to use these

--- driver/wormhole/four-fp/four-fp.py
+++ driver/wormhole/four-fp/four-fp.py
@@ -1,12 +1,39 @@
 from driver.wormhole.tt_metal import TT_Metal
 from driver.wormhole.wormhole_driver import launch_firmware, run_kernel
-from tt_sim.device.tt_device import Wormhole
+from tt_sim.device.tt_device import DeviceTileDiagnostics, Wormhole
+from tt_sim.pe.tensix.util import TensixCoprocessorDiagnostics
 from tt_sim.util.conversion import (
     conv_to_bytes,
     conv_to_float,
 )
 
-wormhole = Wormhole()
+# These diagnostics are optional to the Wormhole (if omitted then all
+# assumed to be off). Put here explicitly so can see how can turn specific
+# reporting on and off
+
+coprocessor_diagnostics = TensixCoprocessorDiagnostics(
+    unpacking=False,
+    packing=False,
+    configurations_set=False,
+    issued_instructions=False,
+    fpu_calculations=False,
+    sfpu_calculations=False,
+    thcon=False,
+)
+
+tile_diags = DeviceTileDiagnostics(
+    brisc_diagnostics=False,
+    ncrisc_diagnostics=False,
+    trisc0_diagnostics=False,
+    trisc1_diagnostics=False,
+    trisc2_diagnostics=False,
+    noc0_diagnostics=False,
+    noc1_diagnostics=False,
+    coprocessor_diagnostics=coprocessor_diagnostics,
+)
+
+
+wormhole = Wormhole(tile_diags)
 tt_metal = TT_Metal("tt_metal_0.62.2.json")
 launch_firmware(wormhole, tt_metal)
 
